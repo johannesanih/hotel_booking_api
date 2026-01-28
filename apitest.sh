164fb36f1fe793fb70f18406bdf5bc8d57da07ba
ibukunanih <ibukunanih@gmail.com>
2026-01-28 20:03:45 +0000

    ran some test

--- apitest.sh
+++ apitest.sh
@@ -4,7 +4,7 @@ URL="https://improved-robot-p4jg6w5rqx9f6x9j-8000.app.github.dev/api/login/"
 
 echo "Fetching data from $URL..."
 
-curl -i -X POST "$URL" \
+curl -s -X POST "$URL" \
     -H "Content-Type: application/json" \
     -H "Accept: application/json" \
     -d "{\"email\": \"john@example.com\", \"password\": \"000000\"}"
